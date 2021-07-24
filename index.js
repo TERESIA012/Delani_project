dfa747680f3e97669e2618e91d433cc3f4389bcb
TERESIA012 <kingoriteresia@gmail.com>
2021-07-24 16:17:38 +0300

    add toggle

---
+++ index.js
@@ -0,0 +1,15 @@
+//what we do section
+
+$(document).ready(function(){
+    $(".pmi").on("click",function(){
+        $(".product-managements").toggle();
+    });
+    $(".desi").on("click",function(){
+        $(".design1").toggle();
+    });
+    $(".devi").on("click",function(){
+        $(".dev").toggle();
+    });
+
+
+});
